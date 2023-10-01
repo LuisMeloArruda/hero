7a7756f16eff76d5df8c1cc42e2ef6968959c241
Luís Arruda <up202206970@up.pt>
2023-10-01 16:22:59 +0100

    Made a screen with laterna

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com.up202206970"
@@ -12,8 +13,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    //implementation group("com.googlecode.lanterna"); name("lanterna"); version("3.1.1")
+    implementation("com.googlecode.lanterna:lanterna:3.1.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("Application")
 }
---
+++ src/main/java/Application.java
@@ -0,0 +1,39 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        Screen screen = null;
+        try {
+
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+
+            screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        terminalFactory.createTerminal();
+        */
+
+        assert screen != null;
+        screen.clear();
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+}
