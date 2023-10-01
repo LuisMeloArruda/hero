39b27e7b693fb63f6d69c08a48168538c7ab3271
Luís Arruda <up202206970@up.pt>
2023-10-01 17:51:00 +0100

    Reading keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,6 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,8 +10,9 @@ import java.io.IOException;
 
 
 public class Game {
+    private int horizontal = 10;
+    private int vertical = 10;
     private Screen screen = null;
-
     Game() {
         try {
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
@@ -25,13 +28,43 @@ public class Game {
         }
     }
 
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowUp:
+                vertical--;
+                break;
+            case ArrowDown:
+                vertical++;
+                break;
+            case ArrowRight:
+                horizontal++;
+                break;
+            case ArrowLeft:
+                horizontal--;
+                break;
+        }
+    }
+
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(horizontal, vertical, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
     public void run() throws IOException {
-        draw();
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getCharacter() != null) {
+                if (key.getCharacter() == 'q') {
+                    screen.close();
+                    break;
+                }
+            }
+            if (key.getKeyType() == KeyType.EOF) {
+                break;
+            }
+            processKey(key);
+        }
     }
     public static void main(String[] args) throws IOException {
         Game game = new Game();
